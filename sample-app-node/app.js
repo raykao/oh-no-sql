7c189b04edf6bbe954352708ce9f708f0ef9f7f6
Raymond Kao <rakao@microsoft.com>
2019-05-21 17:47:38 -0500

    simply routing and add clarity

--- sample-app-node/app.js
+++ sample-app-node/app.js
@@ -1,12 +1,12 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+const Routes = require('./routes');
 
-var app = express();
+const app = express();
 
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +20,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// All routing rules are found in <sample-app-node>/routes
+app.use('/', Routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
